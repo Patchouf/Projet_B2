c45bc2b1b65a55aa1cb815fb035f2250b410a07e
padriana <adriana.pullig@ynov.com>
2024-06-13 19:23:29 +0200

    script

--- API_Projet_Final_B2/installationDotNet.sh
+++ API_Projet_Final_B2/installationDotNet.sh
@@ -6,4 +6,4 @@ sudo dpkg -i packages-microsoft-prod.deb
 rm packages-microsoft-prod.deb
 sudo apt-get update && sudo apt-get install -y dotnet-sdk-8.0
 sudo apt-get update && sudo apt-get install -y aspnetcore-runtime-8.0
-sudo apt-get install -y dotnet-runtime-8.0
+sudo apt-get install -y dotnet-runtime-8.0 
---
+++ BDD/BDD_Projet_Final_B2/data_base.py
@@ -0,0 +1,6 @@
+import pandas as pd
+
+lines=[]
+
+with open('Combined_Authors.csv', 'Combined_Books.csv')as file:
+    file.writelines(lines)
